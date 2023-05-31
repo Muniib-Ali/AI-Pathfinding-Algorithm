6b71968b04368af47cdd4fc6258e6923754dd687
Muniib-Ali <muniib11@live.com>
2023-05-31 11:23:15 +0100

    Can now place start and goal cells

--- pathfind.py
+++ pathfind.py
@@ -4,7 +4,8 @@ cellWidth = 10
 screenWidth = 1000
 numberOfCells = int(screenWidth / cellWidth)
 board = []
-
+start = []
+goal = []
 
 class Cell:
     def __init__(self, colour, x,y):
@@ -27,13 +28,10 @@ class Cell:
     def makeSearched(self):
         self.colour = "grey"
 
-    def makeCanSearch(self):
+    def makeSearchable(self):
         self.colour = "yellow"
         
-def drawGrid():
-  
-    frame.fill("white")
-
+def fillGrid():
     for i in range(numberOfCells):
         board.append([])
 
@@ -43,26 +41,44 @@ def drawGrid():
 
             else:
                 board[i].append(Cell("white", 1 * i, 1 * j))
+
+def drawGrid():
+    frame.fill("white")
     
     for i in board:
         for j in i:
             pygame.draw.rect(frame, j.colour, (j.x * cellWidth , j.y * cellWidth, cellWidth, cellWidth))
     
-
     for i in range(numberOfCells):
         pygame.draw.line(frame, "black", (cellWidth * i,0), (cellWidth * i, screenWidth))
         pygame.draw.line(frame, "black", (0, cellWidth * i), (screenWidth, cellWidth * i))
 
+    pygame.display.flip()
 
+def placeCells():
+    if pygame.mouse.get_pressed()[0]:
+
+        (mouseX, mouseY) = pygame.mouse.get_pos() 
+        mouseX = mouseX - (mouseX % cellWidth)
+        mouseY = mouseY - (mouseY % cellWidth)
+        cell = board[int(mouseX / cellWidth)][int(mouseY / cellWidth)]
+        
+        if not start and cell not in goal and cell.x != 0 and cell.x != (numberOfCells-1) and cell.y != 0  and cell.y != (numberOfCells-1): 
+            cell.makeStart()
+            start.append(cell)
+
+        elif not goal and cell not in start and cell.x != 0 and cell.x != (numberOfCells-1) and cell.y != 0  and cell.y != (numberOfCells-1):
+            cell.makeGoal()
+            goal.append(cell)
+        
+        elif cell not in goal and cell not in start:
+            cell.makeWall()
     
-
-            
-
 pygame.init()
-
 frame = pygame.display.set_mode((screenWidth, screenWidth))
 pygame.display.set_caption('Pathfinding Algorithm')
 running = True
+fillGrid()
 
 while running:
 
@@ -71,6 +87,4 @@ while running:
             running = False
 
     drawGrid()
-    pygame.display.flip()
-  
-
+    placeCells()
